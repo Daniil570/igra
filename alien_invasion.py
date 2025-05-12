e50ee6ee2f7d5f7eb12fc5e25b3fdd619497041e
Daniil570 <daniilmatashkin@gmail.com>
2025-05-12 09:59:12 +0300

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -7,20 +8,24 @@ import game_functions as gf
 def run_game():
     pygame.init()
     ai_settings = Settings()
-
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
     
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
     bg_color = (230, 230, 230)
 
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        bullets.update()
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
