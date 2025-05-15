be74aefa741c39c5f53f23be1af2c6095d189791
Daniil570 <daniilmatashkin@gmail.com>
2025-05-15 11:46:46 +0300

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,36 +3,43 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     
+    
     screen = pygame.display.set_mode((1200, 800))
+
     pygame.display.set_caption("Alien Invasion")
+    play_button = Button(ai_settings, screen, "Play")
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    bg_color = (230, 230, 230)
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        
         bullets.update()
         for bullet in bullets.copy():
             if bullet.rect.bottom <= 0:
                 bullets.remove(bullet)
-        print(len(bullets))
-        gf.update_screen(ai_settings, screen, ship, bullets)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(bg_color)
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
