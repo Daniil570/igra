2aa4b9d98e4447c7413aa6f682a4ab16ead0b3fb
Daniil570 <daniilmatashkin@gmail.com>
2025-05-15 11:50:19 +0300

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -24,8 +24,8 @@ class Scoreboard():
     def show_score(self):
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
         self.ships.draw(self.screen)
-        self.level_rect.top = self.score_rect.bottom + 10
         
     def prep_high_score(self):
         high_score = int(round(self.stats.high_score, -1))
